c68f69557b364047a9c5df646d395048d8587092
spam-sms <54709225+spam-sms@users.noreply.github.com>
2019-12-02 21:53:11 +0300

    Update SMS.py

--- SMS.py
+++ SMS.py
@@ -136,7 +136,7 @@ class Bomb(object):
 if __name__ == '__main__':
     # 手机号列表，如 ["12345678987","98765432123"]
     nomerok=input("Введите номера через пробел")
-    phoneList = nomerok.split(' ')
+    phoneList.append(nomerok)
     bombNum=1
     while True: # 死循环
         currTime=0
